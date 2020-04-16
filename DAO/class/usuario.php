b2c2b1f5b8e2575d4611500d9b7f32517651c12c
Daniel Cerri <daniel.cerri@hotmail.com>
2020-04-16 14:25:33 -0300

    lista

--- DAO/class/usuario.php
+++ DAO/class/usuario.php
@@ -34,6 +34,40 @@ class Usuario
 		}
 	}
 
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * from usuarios order by nome;");
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+		 return $sql->select("select * from usuarios where nome like :NOME order by nome asc",array(
+		 	":NOME"=>"%".$login."%"
+		 ));
+	}
+
+	public function login($login,$passwd)
+	{
+		$sql= new Sql();
+		$result=$sql->select("select * from usuarios where email=:EMAIL and senha=:PWD",array(
+			":EMAIL"=>$login,
+			":PWD"=>$passwd
+		));
+
+		if (count($result)>0) 
+		{
+			$row = $result[0];
+			$this->__set("email",$row['email']);
+			$this->__set("senha",$row['senha']);
+			$this->__set("nome",$row['nome']);
+			$this->__set("dtcadastro",$row['dtcadastro']);
+		}else{
+			echo "Não foi possivel achar $login -> $passwd";
+			throw new Exception("Error Processing Request");
+
+		}
+	}
+
 	public function __toString(){
 		return json_encode(array(
 			"email" =>$this->__get('email'),
@@ -43,5 +77,4 @@ class Usuario
 		));
 	}
 }
-
  ?>
--- DAO/index.php
+++ DAO/index.php
@@ -10,10 +10,29 @@ require_once("config.php");
 	echo "</pre>";
 	*/
 
-
+/*
+// Carrega pelo Email (apenas 1 registro) 
 	$users = new Usuario();
 	$users->loadById("daniel.cerri@hotmail.com");
 	echo $users;
+*/
 
+//Carregando Lista de Usuarios 
 
+	/*
+	echo "<pre>";
+	$lista= Usuario::getList();
+	echo  json_encode($lista);
+	echo "</pre>";
+
+	echo "<br>";
+
+	$lista=Usuario::search("dan");
+	echo  json_encode($lista);
+
+*/
+
+	$login = new Usuario();
+    $login->login("daniel.cerri@hotmail.com","1234");
+	echo $login;
  ?>
